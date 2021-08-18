de1a4985bb0eb668821a999b41456872d35f72d1
Giovanny Peñaloza <jgpenaloza2@misena.edu.co>
2021-08-18 13:16:43 -0500

    Crear, detalle,eliminar,editar

--- Cortana/Controllers/UsuarioController.cs
+++ Cortana/Controllers/UsuarioController.cs
@@ -22,7 +22,7 @@ namespace Cortana.Controllers
             }
 
         }
-
+        //crear//
         public ActionResult Create()
         {
             return View();
@@ -51,6 +51,83 @@ namespace Cortana.Controllers
 
             }
             catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //Detalles//
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.usuario.Find(id);
+                return View(findUser);
+            }
+        }
+        //eliminar//
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.usuario.Find(id);
+                    db.usuario.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //editar//
+
+        public ActionResult Edit (int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuario editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
             {
                 ModelState.AddModelError("", "error" + ex);
                 return View();
