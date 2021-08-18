85d5b13c0e8155ceb196d78f9da81a41e5bcefdb
Giovanny Peñaloza <jgpenaloza2@misena.edu.co>
2021-08-18 14:41:56 -0500

    ClienteController

---
+++ Cortana/Controllers/ClienteController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cortana.Models;
+
+namespace Cortana.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var bd = new inventario2021Entities())
+
+            {
+
+
+                return View(bd.cliente.ToList());
+
+            }
+        }
+
+        //crear//
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+
+
+            if (!ModelState.IsValid)
+                return View();
+
+
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //Detalles//
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.cliente.Find(id);
+                return View(findUser);
+            }
+        }
+
+        //eliminar//
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.cliente.Find(id);
+                    db.cliente.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //editar
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente findUser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente user = db.cliente.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.documento = editUser.documento;
+                    user.email = editUser.email;
+                  
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
